59411908dec80d0ecd8e2cb1418b42f19e1ffa75
xa <87640952+xaywj@users.noreply.github.com>
2023-09-19 22:35:15 +0700

    config database

---
+++ src/database/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'mysql', // Replace with your database type
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'chanllenge_db',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true, // Set to false in production
+};
