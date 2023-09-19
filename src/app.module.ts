1b783d4c1e926ff0156d391e6ad31857bef05ea0
xa <87640952+xaywj@users.noreply.github.com>
2023-09-19 23:10:39 +0700

    create file crude user and ganerate table user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
   imports: [
-    DatabaseModule
+    DatabaseModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
