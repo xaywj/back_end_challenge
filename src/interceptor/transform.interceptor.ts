ac2ee03bc7a89ec038e070879e5c83813f660a19
xa <87640952+xaywj@users.noreply.github.com>
2023-09-21 23:39:08 +0700

    create function crud rate

--- src/interceptor/transform.interceptor.ts
+++ src/interceptor/transform.interceptor.ts
@@ -1,30 +1,43 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export interface Response<T> {
   data: {
-    data:any;
-    error:string;
-    message:string;
-    status:number;
-    path:string;
+    data: any;
+    error: string;
+    message: string;
+    status: number;
+    path: string;
   };
 }
 
 @Injectable()
-export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
     const req = context.switchToHttp().getRequest();
-    const res= context.switchToHttp().getResponse();
-    return next.handle().pipe(map(data => ({ 
+    const res = context.switchToHttp().getResponse();
+    res.status(200);
+    return next.handle().pipe(
+      map((data) => ({
         data: {
-            data:data,
-            error:res.statusMessage,
-            message:res.message,
-            status:200,
-            path:req.url,
-        }
-     })));
+          data: data,
+          error: res.statusMessage,
+          message: res.message,
+          status: 200,
+          path: req.url,
+        },
+      })),
+    );
   }
 }
